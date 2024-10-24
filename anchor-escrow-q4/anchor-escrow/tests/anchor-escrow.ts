a46bec6ddcd9ef872cad99cc9e81ed7f1fba7b41
fraanfx <fran.am95@gmail.com>
2024-10-24 12:32:40 +0200

    Add escrow testing test

--- anchor-escrow-q4/anchor-escrow/tests/anchor-escrow.ts
+++ anchor-escrow-q4/anchor-escrow/tests/anchor-escrow.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorEscrow } from "../target/types/anchor_escrow";
+import { use } from "chai";
 
 describe("anchor-escrow", () => {
   // Configure the client to use the local cluster.
@@ -9,6 +10,14 @@ describe("anchor-escrow", () => {
 
   const program = anchor.workspace.AnchorEscrow as Program<AnchorEscrow>;
 
+  // Initializing escrow dependencies
+
+  const escrowState =  anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("state"), provider.publicKey.toBytes()], program.programId)[0];
+
+  const escrow = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("state"), escrowState.toBytes()], program.programId)[0];
+
+  // Initializing tests
+
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
@@ -16,9 +25,22 @@ describe("anchor-escrow", () => {
   });
 
 
-  it("Is initialized!", async () => {
+  it("Make a deposit 4 sol", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+    .make(
+      new ancho.BN(4 * anchor.web3.LAMPORTS_PER_SOL)
+    )
+    .accountsPartial(
+      {
+        marker: provider.wallet.publicKey,
+        escrowState,
+        escrow,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      }
+    )
+    .rpc();
     console.log("Your transaction signature", tx);
+    console.log("Escrow deposit: ", (await provider.connection.getAccountInfo(escrow)));
   });
 });
